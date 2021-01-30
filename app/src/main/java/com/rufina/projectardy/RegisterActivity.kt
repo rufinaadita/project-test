8f8717ef50020edba92128b540e47b0a92f01441
ArdyJunata <ardyjunata53@gmail.com>
2021-01-30 20:05:53 +0700

    register selesai

---
+++ app/src/main/java/com/rufina/projectardy/RegisterActivity.kt
@@ -0,0 +1,10 @@
+package com.rufina.projectardy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
